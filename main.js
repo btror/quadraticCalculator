6d39b58b6effcddd8f669c38d5d5507b923b7c5b
Brandon <50763955+btror@users.noreply.github.com>
2020-02-22 19:04:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function quadraticFunc() {
 
     var int1;
     var int2;
-    
+
 
     if (a != 0) {
         var num1 = ((b * -1) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
@@ -46,35 +46,32 @@ function quadraticFunc() {
         int2 = 0 / 0;
     }
 
-    if (int1 == int2 || b == 0) {
+    if (int1 == int2) { // remove || b == 0
         int2 = 0 / 0;
     }
 
-    
+
 
     var status1 = Number.isNaN(int1);
     var status2 = Number.isNaN(int2);
     var inttemp;
-    if (int1 > int2 && status1 == false && status2 == false) {
+    if (Math.abs(int1) > Math.abs(int2) && status1 == false && status2 == false) {
         if (int1 < 0) {
             inttemp = int1 * -1;
-            globalScale = 100/inttemp;
+            globalScale = 100 / inttemp;
         } else {
-            globalScale = 100/int1;
+            globalScale = 100 / int1;
         }
 
         if (int1 == 0 || inttemp < 5) {
             globalScale = 5;
         }
-
-        
-        
-    } else if (int1 < int2 && status1 == false && status2 == false){
+    } else if (Math.abs(int1) < Math.abs(int2) && status1 == false && status2 == false) {
         if (int2 < 0) {
             inttemp = int2 * -1;
-            globalScale = 100/inttemp;
+            globalScale = 100 / inttemp;
         } else {
-            globalScale = 100/int2;
+            globalScale = 100 / int2;
         }
         if (int2 == 0 || inttemp < 5) {
             globalScale = 5;
@@ -83,9 +80,9 @@ function quadraticFunc() {
     } else if (status1 == false && status2 == true) {
         if (int1 < 0) {
             inttemp = int1 * -1;
-            globalScale = 100/inttemp;
+            globalScale = 100 / inttemp;
         } else {
-            globalScale = 100/int1;
+            globalScale = 100 / int1;
         }
         if (int1 == 0 || inttemp < 5) {
             globalScale = 5;
@@ -93,9 +90,9 @@ function quadraticFunc() {
     } else if (status1 == true && status2 == false) {
         if (int2 < 0) {
             inttemp = int2 * -1;
-            globalScale = 100/inttemp;
+            globalScale = 100 / inttemp;
         } else {
-            globalScale = 100/int2;
+            globalScale = 100 / int2;
         }
         if (int2 == 0 || inttemp < 5) {
             globalScale = 5;
@@ -103,13 +100,13 @@ function quadraticFunc() {
     } else {
         //compare b and c values
         if (c > a) {
-            globalScale = 100/(c*3);
+            globalScale = 100 / (c * 3);
         } else if (c < a) {
-            globalScale = 100/(a*3);
+            globalScale = 100 / (a * 3);
         } else {
             globalScale = 5;
         }
-        
+
     }
 
     var letters = false;
@@ -120,7 +117,7 @@ function quadraticFunc() {
     var x;
 
     if (status1 == false && status2 == false && letters == false) {
-        document.getElementById("output").innerHTML = "Intercept at x = " + int2 + " and x = " + int1;
+        document.getElementById("output").innerHTML = "<u>Intercepts</u>:<br><br>x = " + int2 + "<br>x = " + int1;
         document.getElementById("output2").innerHTML = a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
         fun1(x);
         draw(x);
@@ -130,50 +127,80 @@ function quadraticFunc() {
 
         document.getElementById("problem").innerHTML = "Find the solution for:<br>" + a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
         document.getElementById("listHow").innerHTML = "Using the quadratic formula where a = " + a + ", b = " + b + ", c = " + c;
-        document.getElementById("steps").innerHTML = "x = (-(" + b + ") ± √((-(" + b + "))<sup>2</sup> - 4(" + a + ")(" + c + "))) / (2(" + a + "))<br>"
-            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - 4(" + a + ")(" + c + "))) / (2(" + a + "))<br>"
-            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - (" + (4 * a * c) + "))) / (" + 2 * a + ")<br>"
-            + "x = (" + (b * -1) + " ± √(" + (Math.pow(b, 2) + (-1 * (4 * a * c))) + ")) / (" + 2 * a + ")<br><br>"
+        document.getElementById("steps").innerHTML = "x = (-(" + b + ") ± √((-(" + b + "))<sup>2</sup> - 4(" + a + ")(" + c + "))) / 2(" + a + ")<br>"
+            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - 4(" + a + ")(" + c + "))) / 2(" + a + ")<br>"
+            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - (" + (4 * a * c) + "))) / " + 2 * a + "<br>"
+            + "x = (" + (b * -1) + " ± √(" + (Math.pow(b, 2) + (-1 * (4 * a * c))) + ")) / " + 2 * a + "<br><br>"
             + "root at x = (" + (b * -1) + " + " + Math.sqrt(Math.pow(b, 2) - 4 * a * c) + ") / " + 2 * a + " = " + ((b * -1) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a) + "<br>"
             + "root at x = (" + (b * -1) + " - " + Math.sqrt(Math.pow(b, 2) - 4 * a * c) + ") / " + 2 * a + " = " + ((b * -1) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
 
 
-    } else if (status1 == false && status2 == true && letters == false) {
-        document.getElementById("output").innerHTML = "Intercept at x = " + int1;
-        document.getElementById("output2").innerHTML = a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
-        fun1(x);
-        draw(x);
-        document.getElementById("output3").innerHTML = a + "(" + int1 + ")" + "<sup>2</sup> + " + b + "(" + int1 + ") + " + c + " = 0";
-        document.getElementById("output4").innerHTML = "";
-        document.getElementById("output5").innerHTML = "F(x) = " + a + "x<sup>2</sup> + " + b + "x + " + c;
+    } else if (status1 == false && status2 == true && letters == false) { //not a quadratic
 
-        document.getElementById("problem").innerHTML = "Find the solution for:<br>" + a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
-        document.getElementById("listHow").innerHTML = "Using the quadratic formula where a = " + a + ", b = " + b + ", c = " + c;
-        document.getElementById("steps").innerHTML = "x = (-(" + b + ") ± √((-(" + b + "))<sup>2</sup> - 4(" + a + ")(" + c + "))) / (2(" + a + "))<br>"
-            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - 4(" + a + ")(" + c + "))) / (2(" + a + "))<br>"
-            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - (" + (4 * a * c) + "))) / (" + 2 * a + ")<br>"
-            + "x = (" + (b * -1) + " ± √(" + (Math.pow(b, 2) + (-1 * (4 * a * c))) + ")) / (" + 2 * a + ")<br><br>"
-            + "root at x = (" + (b * -1) + " + " + Math.sqrt(Math.pow(b, 2) - 4 * a * c) + ") / " + 2 * a + " = " + ((b * -1) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a) + "<br>"
-            + "root at x = (" + (b * -1) + " - " + Math.sqrt(Math.pow(b, 2) - 4 * a * c) + ") / " + 2 * a + " = " + ((b * -1) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        if (isFinite(int1)) {
+            document.getElementById("output").innerHTML = "<u>Intercept:</u><br><br>x = " + int1;
+            document.getElementById("output2").innerHTML = a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
+            fun1(x);
+            draw(x);
+            document.getElementById("output3").innerHTML = a + "(" + int1 + ")" + "<sup>2</sup> + " + b + "(" + int1 + ") + " + c + " = 0";
+            document.getElementById("output4").innerHTML = "";
+            document.getElementById("output5").innerHTML = "F(x) = " + a + "x<sup>2</sup> + " + b + "x + " + c;
 
-    } else if (status1 == true && status2 == false && letters == false) {
-        document.getElementById("output").innerHTML = "Intercept at x = " + int2;
-        document.getElementById("output2").innerHTML = a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
-        fun1(x);
-        draw(x);
-        document.getElementById("output3").innerHTML = a + "(" + int2 + ")" + "<sup>2</sup>+" + b + "(" + int2 + ")+" + c + "=0";
-        document.getElementById("output4").innerHTML = "";
-        document.getElementById("output5").innerHTML = "F(x) = " + a + "x<sup>2</sup> + " + b + "x + " + c;
+            document.getElementById("problem").innerHTML = "Solve for x:<br><br>" + b + "x + " + c + " = 0";
+            document.getElementById("listHow").innerHTML = "Since a = 0, the quadratic formula cannot be used";
+            document.getElementById("steps").innerHTML = "(" + b + ")x + " + c + " = 0<br>"
+                + "(" + b + ")x = " + "0 - (" + c + ")<br>"
+                + "x = " + (-1 * c) + " / " + b + "<br>"
+                + "x = " + (-1 * c) / b;
+            quadf.style.display = "none";
+        } else {
+            document.getElementById("output").innerHTML = "<u>Intercept:</u><br><br>There are no intercepts";
+            document.getElementById("output2").innerHTML = c + " = 0";
+            fun1(x);
+            draw(x);
+            document.getElementById("output3").innerHTML = "";
+            document.getElementById("output4").innerHTML = "";
+            document.getElementById("output5").innerHTML = "F(x) = " + c;
 
-        document.getElementById("problem").innerHTML = "Find the solution for:<br>" + a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
-        document.getElementById("listHow").innerHTML = "Using the quadratic formula where a = " + a + ", b = " + b + ", c = " + c;
-        document.getElementById("steps").innerHTML = "x = (-(" + b + ") ± √((-(" + b + "))<sup>2</sup> - 4(" + a + ")(" + c + "))) / (2(" + a + "))<br>"
-            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - 4(" + a + ")(" + c + "))) / (2(" + a + "))<br>"
-            + "x = (" + (b * -1) + " ± √(" + Math.pow(b, 2) + " - (" + (4 * a * c) + "))) / (" + 2 * a + ")<br>"
-            + "x = (" + (b * -1) + " ± √(" + (Math.pow(b, 2) + (-1 * (4 * a * c))) + ")) / (" + 2 * a + ")<br><br>"
-            + "root at x = (" + (b * -1) + " + " + Math.sqrt(Math.pow(b, 2) - 4 * a * c) + ") / " + 2 * a + " = " + ((b * -1) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a) + "<br>"
-            + "root at x = (" + (b * -1) + " - " + Math.sqrt(Math.pow(b, 2) - 4 * a * c) + ") / " + 2 * a + " = " + ((b * -1) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+            document.getElementById("problem").innerHTML = "Solve for x:<br><br>" + b + "x + " + c + " = 0";
+            document.getElementById("listHow").innerHTML = "There is not an intercept";
+            document.getElementById("steps").innerHTML = "";
+            quadf.style.display = "none";
+        }
 
+
+    } else if (status1 == true && status2 == false && letters == false) { //not a quadratic
+
+        if (isFinite(int2)) {
+            document.getElementById("output").innerHTML = "<u>Intercept:</u><br><br>x = " + int2;
+            document.getElementById("output2").innerHTML = a + "x<sup>2</sup> + " + b + "x + " + c + " = 0";
+            fun1(x);
+            draw(x);
+            document.getElementById("output3").innerHTML = a + "(" + int2 + ")" + "<sup>2</sup>+" + b + "(" + int2 + ")+" + c + "=0";
+            document.getElementById("output4").innerHTML = "";
+            document.getElementById("output5").innerHTML = "F(x) = " + a + "x<sup>2</sup> + " + b + "x + " + c;
+
+            document.getElementById("problem").innerHTML = "Solve for x:<br><br>" + b + "x + " + c + " = 0";
+            document.getElementById("listHow").innerHTML = "Since a = 0, the quadratic formula cannot be used";
+            document.getElementById("steps").innerHTML = "(" + b + ")x + " + c + " = 0<br>"
+                + "(" + b + ")x = " + "0 - (" + c + ")<br>"
+                + "x = " + (-1 * c) + " / " + b + "<br>"
+                + "x = " + (-1 * c) / b;
+            quadf.style.display = "none";
+        } else {
+            document.getElementById("output").innerHTML = "<u>Intercept:</u><br><br>There are no intercepts";
+            document.getElementById("output2").innerHTML = c + " = 0";
+            fun1(x);
+            draw(x);
+            document.getElementById("output3").innerHTML = "";
+            document.getElementById("output4").innerHTML = "";
+            document.getElementById("output5").innerHTML = "F(x) = " + c;
+
+            document.getElementById("problem").innerHTML = "Solve for x:<br><br>" + b + "x + " + c + " = 0";
+            document.getElementById("listHow").innerHTML = "There is not an intercept";
+            document.getElementById("steps").innerHTML = "";
+            quadf.style.display = "none";
+        }
     } else {
         document.getElementById("output").innerHTML = "This function has no real roots.";
         document.getElementById("output2").innerHTML = "";
@@ -197,7 +224,24 @@ function quadraticFunc() {
 }
 
 function fun1(x) {
-    var num = ga * Math.pow(x, 2) + (gb * x) + gc;
+    var num;
+
+    num = ga * Math.pow(x, 2) + (gb * x) + gc;
+
+    if (ga == 0 && gb == 0) {
+        num = gc;
+        if (gc > 0) {
+            gc = 5;
+        } else if (gc < 0) {
+            gc = -5;
+        } else {
+            gc = 0;
+        }
+        
+        globalScale = 10;
+        
+    }
+
     return num;
 }
 
@@ -207,17 +251,10 @@ function draw() {
     if (null == canvas || !canvas.getContext) return;
 
     var axes = {}, ctx = canvas.getContext("2d");
-    axes.x0 = .5 + .5 * canvas.width;  // x0 pixels from left to x=0
+    axes.x0 = .5 + .5 * canvas.width; // x0 pixels from left to x=0
     axes.y0 = .5 + .5 * canvas.height; // y0 pixels from top to y=0
-    
-    // if (firstIntercept > secondIntercept) {
-    //     axes.scale = 100/firstIntercept; 
-    // } else if (firstIntercept < secondIntercept) {
-    //     axes.scale = 100/secondIntercept;
-    // } else {
-    //     axes.scale = 100/firstIntercept;
-    // }
-    axes.scale = globalScale;                // 40 pixels from x=0 to x=1
+
+    axes.scale = globalScale; // 40 pixels from x=0 to x=1
     axes.doNegativeX = true;
 
     showAxes(ctx, axes);
@@ -251,7 +288,7 @@ function showAxes(ctx, axes) {
     ctx.strokeStyle = "rgb(128,128,128)";
     ctx.fillStyle = "lightgray";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.moveTo(xmin, y0); ctx.lineTo(w, y0);  // X axis
-    ctx.moveTo(x0, 0); ctx.lineTo(x0, h);  // Y axis
+    ctx.moveTo(xmin, y0); ctx.lineTo(w, y0); // X axis
+    ctx.moveTo(x0, 0); ctx.lineTo(x0, h); // Y axis
     ctx.stroke();
 }
